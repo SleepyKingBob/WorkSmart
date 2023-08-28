baa95f3c4377a946fdbefe9ea735458f3bb1ebfe
Adrian Schober <adrian.schober1970@gmail.com>
2023-08-28 16:09:10 +0200

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources "projects" do
+    get "projects/:user_id/completed", to: "projects#index_complete"
+    get "projects/:project_id", to: "projects#set_active"
+    resources "tasks", only: %i[new create update destroy]
+  end
+
+  resources "schedules", only: %i[new create edit update destroy]
 
   # Defines the root path route ("/")
   # root "articles#index"
