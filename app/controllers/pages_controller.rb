50ecbc34dd24d13df86215e256a3575298947e0a
Matheus Hubert <matheushubert@gmail.com>
2023-08-30 14:37:28 +0200

    redirect and fix password

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   before_action :set_user, only: %I[dashboard]
 
   def home
+    redirect_to dashboard_path if current_user != nil && current_user.first_name != nil
+    # redirect_to onboard_path if current_user != nil && current_user.first_name == nil
   end
 
   def dashboard
